da89c9d348f752deb1784c5b90906985795ecb7e
zachbaker55 <thezachbaker55@gmail.com>
2023-10-22 13:33:33 -0400

    Collectable coins & Coin/Ammo UI
    Coins are collectable and coin amount and ammo amount now show on the UI

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : PersistentSingleton<GameManager> {
 
     // Properties
+    [SerializeField] private TMP_Text _coinText;
+    [SerializeField] private TMP_Text _ammoText;
+
     [SerializeField] private float _coinAmount;
     public float CoinAmount {
         get { return _coinAmount; }
         set { _coinAmount = value; 
-        Debug.Log("Coins: " + _coinAmount); }
+        _coinText.text = "Coins: " + _coinAmount; }
     }
 
-    // Methods
+    [SerializeField] private float _ammoAmount;
+    public float AmmoAmount {
+        get { return _ammoAmount; }
+        set { _ammoAmount = value; 
+        _ammoText.text  = "Ammo: " + _ammoAmount; }
+    }
+
+    
 
 }
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField] private float _ammoAmount;
+    public float AmmoAmount {
+        get { return _ammoAmount; }
+        set { _ammoAmount = value; }
+    }
+
 }
